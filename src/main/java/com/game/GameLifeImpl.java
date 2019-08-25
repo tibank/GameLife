496d9f9d2deb219c16b29331c6a15ec0dceb5eb4
Vladimir Lysenko <tibank2071@gmail.com>
2019-08-25 21:37:22 +0300

    add change matrix like an infinite

--- src/main/java/com/game/GameLifeImpl.java
+++ src/main/java/com/game/GameLifeImpl.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class GameLifeImpl implements GameLife {
     private final static Logger log = LoggerFactory.getLogger(GameLifeImpl.class);
@@ -26,13 +27,57 @@ public class GameLifeImpl implements GameLife {
 
     private void tick() {
         char[][] chars = initMatrix();
+        boolean isNeedMoveX = false;
+        boolean isNeedMoveY = false;
         for (int i = 0; i < loader.getSizeX(); i++) {
             for (int j = 0; j < loader.getSizeY(); j++) {
-                chars[i][j] = matrix[i][j] == 'X' ? checkAliveCell(i, j) : checkDeadCell(i, j);
+                if (matrix[i][j] == 'X') {
+                    chars[i][j] = checkAliveCell(i, j);
+                    if (i == loader.getSizeX() - 1) {
+                        isNeedMoveY = true;
+                    }
+                    if (j == loader.getSizeY() - 1) {
+                        isNeedMoveX = true;
+                    }
+                } else {
+                    chars[i][j] = checkDeadCell(i, j);
+                }
             }
         }
 
+        if (isNeedMoveX) {
+            moveToLeft(chars);
+        }
+
+        if (isNeedMoveY) {
+            moveToDown(chars);
+        }
+
         matrix = chars;
+    }
+
+    private void moveToDown(char[][] chars) {
+        for (int i = 0; i < loader.getSizeX() - 1; i++) {
+            for (int j = 0; j < loader.getSizeY(); j++) {
+                chars[i][j] = chars[i+1][j];
+            }
+        }
+
+        for (int j = 0; j < loader.getSizeY(); j++) {
+            chars[loader.getSizeX() - 1][j] = 'O';
+        }
+    }
+
+    private void moveToLeft(char[][] chars) {
+        for (int j = 0; j < loader.getSizeY() - 1; j++) {
+            for (int i = 0; i < loader.getSizeX(); i++) {
+                chars[i][j] = chars[i][j+1];
+            }
+        }
+
+        for (int i = 0; i < loader.getSizeX(); i++) {
+            chars[i][loader.getSizeY() - 1] = 'O';
+        }
     }
 
     private char checkAliveCell(int x, int y) {
@@ -102,7 +147,7 @@ public class GameLifeImpl implements GameLife {
                 bufferedWriter.newLine();
             }
         } catch (IOException e) {
-            throw new ApplicationException("Error writing result to file",e);
+            throw new ApplicationException("Error writing result to file", e);
         }
     }
 
@@ -126,7 +171,26 @@ public class GameLifeImpl implements GameLife {
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < loader.getSizeX(); i++) {
             for (int j = 0; j < loader.getSizeY(); j++) {
-                result.append(matrix[i][j]);
+                if (matrix[i][j] == 'X') {
+                    //result.append('\u25a0');
+                    result.append('\u2589');
+                } else {
+                    //result.append('\u25a1');
+                    result.append('\u2591');
+                }
+
+            }
+            result.append("\n");
+        }
+
+        return result.toString();
+    }
+
+    private String printMatrix(char[][] chars) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < loader.getSizeX(); i++) {
+            for (int j = 0; j < loader.getSizeY(); j++) {
+                result.append(chars[i][j]);
             }
             result.append("\n");
         }
